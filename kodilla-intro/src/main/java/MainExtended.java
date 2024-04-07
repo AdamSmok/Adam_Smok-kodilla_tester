e7720a4381fb2022af2ef3a2393bb59368cedf74
Adam Smok <adam.smok-pieniazek@outlook.com>
2024-04-07 18:17:48 +0200

    Dopisanie wywietlania w MainExtended

--- kodilla-intro/src/main/java/MainExtended.java
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -1,10 +1,17 @@
 public class MainExtended {
     public static void main(String[] args) {
         String myName = "Adam";
-        String city = "Łódź";
+        String city = "Lodz";
         int myAge = 48;
         double height = 1.76D;
         char carClass = 'E';
         boolean isGoodProgrammer = false;
+
+        System.out.println("Imie: " + myName);
+        System.out.println("Miasto: " + city);
+        System.out.println("Wiek: " + myAge);
+        System.out.println("Wzrost: " + height);
+        System.out.println("Auto: " + carClass);
+        System.out.println("Czy jest dobrym programistą: " + isGoodProgrammer);
     }
 }
