b2b4ed1a8e6a2bda35c8cd5c416fd437cc8c745e
Adam Smok <adam.smok-pieniazek@outlook.com>
2024-04-07 18:52:11 +0200

    Wersja z pętlą

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class LeapYear {
 
     public static void main(String[] args) {
-        System.out.println("Podaj rok do sprawdzenia.");
+        /*System.out.println("Podaj rok do sprawdzenia.");
         //pobieram dane od użytkownika
         Scanner read = new Scanner(System.in);
         int year = read.nextInt();
@@ -12,7 +12,11 @@ public class LeapYear {
             System.out.println("Podany rok jest przestepny.");
         } else {
             System.out.println("Podany rok nie jest przestepny.");
+        }*/
+        for (int year = 1976; year < 2030; year++) {
+            if (leap(year)) System.out.println("Podany rok jest przestepny: " + year);
         }
+
     }
 
     //sprawdzam czy rok jest przestępny
