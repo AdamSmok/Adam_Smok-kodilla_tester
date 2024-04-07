c4709840f2fc01fd02db6a258a63875275e26792
Adam Smok <adam.smok-pieniazek@outlook.com>
2024-04-07 17:08:00 +0200

    Hello world

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Moje imię to: Adam.");
+        System.out.println("Mój wiek: 48 lat.");
+        System.out.println("Moje nazwisko zaczyna się na S.");
+    }
+}
