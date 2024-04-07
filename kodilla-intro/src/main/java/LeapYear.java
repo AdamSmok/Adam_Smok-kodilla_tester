4fb7f8373f91fb81deb5b266654e7ec4e3bcc46f
Adam Smok <adam.smok-pieniazek@outlook.com>
2024-04-07 18:36:56 +0200

    Rok Przestpny

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class LeapYear {
+    //sprawdzam czy rok jest przestępny
+    private static boolean leap(int year) {
+        return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+    }
+
+    //pobieram dane od użytkownika
+    public static void main(String[] args) {
+        System.out.println("Podaj rok do sprawdzenia.");
+        Scanner read = new Scanner(System.in);
+        int year = read.nextInt();
+
+        if (leap(year)) {
+            System.out.println("Podany rok jest przestepny.");
+        } else {
+            System.out.println("Podany rok nie jest przestepny.");
+        }
+    }
 }
+
