eccd21b80a34b8abf87ab1001f693606d589fe8d
Adam Smok <adam.smok-pieniazek@outlook.com>
2024-04-07 18:07:01 +0200

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,10 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Adam";
+        String city = "Łódź";
+        int myAge = 48;
+        double height = 1.76D;
+        char carClass = 'E';
+        boolean isGoodProgrammer = false;
+    }
+}
