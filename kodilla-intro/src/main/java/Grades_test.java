92649d5ee33881b119fe2546165e156042db47d7
Adam Smok <adam.smok-pieniazek@outlook.com>
2024-04-11 20:27:01 +0200

    Zadanie z modulu 2.4

---
+++ kodilla-intro/src/main/java/Grades_test.java
@@ -0,0 +1,21 @@
+public class Grades_test {
+    public static void main(String[] args) {
+        Grades student1 = new Grades();
+        student1.add(3);
+        student1.add(2);
+        student1.add(5);
+        student1.add(2);
+        student1.add(3);
+        student1.add(2);
+        student1.add(4);
+        student1.add(5);
+        student1.add(6);
+        student1.add(4);
+        //Zgodnie z treścią zadania następne oceny powinny być ignorowane
+        student1.add(6);
+        student1.add(1);
+
+        System.out.println("Ostatnia ocena: " + student1.lastScore());
+        System.out.println("Średnia ocen  : " + student1.avg());
+    }
+}
