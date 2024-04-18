d3b544e3886ddb48377219ffaa29c476646ab7b8
Adam Smok <adam.smok-pieniazek@outlook.com>
2024-04-18 19:37:03 +0200

    Ładny kod

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -2,22 +2,18 @@ public class FirstClass {
     public static void main(String[] args) {
 
         Notebook notebook = new Notebook(600, 800, 2024);
+        Notebook heavyNotebook = new Notebook(2000, 1500, 2023);
+        Notebook oldNotebook = new Notebook(1600, 500, 2022);
+
+        notebookParameters(notebook);
+        notebookParameters(heavyNotebook);
+        notebookParameters(oldNotebook);
+    }
+
+    private static void notebookParameters(Notebook notebook) {
         System.out.println("\nWaga: " + notebook.weight + " Cena: " + notebook.price + " Rocznik: " + notebook.year);
         notebook.checkPrice();
         notebook.checkWeight();
         notebook.rateNotebook();
-
-        Notebook heavyNotebook = new Notebook(2000, 1500, 2023);
-        System.out.println("\nWaga: " + heavyNotebook.weight + " Cena: " + heavyNotebook.price + " Rocznik: " + heavyNotebook.year);
-        heavyNotebook.checkPrice();
-        heavyNotebook.checkWeight();
-        heavyNotebook.rateNotebook();
-
-        Notebook oldNotebook = new Notebook(1600, 500, 2022);
-        System.out.println("\nWaga: " + oldNotebook.weight + " Cena: " + oldNotebook.price + " Rocznik: " + oldNotebook.year);
-        oldNotebook.checkPrice();
-        oldNotebook.checkWeight();
-        oldNotebook.rateNotebook();
-
     }
 }
