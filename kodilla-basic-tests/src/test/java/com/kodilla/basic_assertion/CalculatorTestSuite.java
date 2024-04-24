6f5b44b98d621c6df24f1f049af7fb4b4d644ce0
Adam Smok <adam.smok-pieniazek@outlook.com>
2024-04-24 20:19:10 +0200

    Modul3.6 dodanie @BeforeEach

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -1,41 +1,44 @@
 package com.kodilla.basic_assertion;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTestSuite {
+    Calculator calculator;
+    int a,b;
+    double c,d;
+
+    @BeforeEach
+    public void setUp(){
+        calculator = new Calculator();
+        a = 5;
+        b = 8;
+        c = 2;
+        d = 0;
+    }
+
     @Test
     public void testAdd() {
-        Calculator calculator = new Calculator();
-        int a = 5;
-        int b = 8;
         int addResult = calculator.add(a, b);
         assertEquals(13, addResult);
     }
 
     @Test
     public void testSub() {
-        Calculator calculator = new Calculator();
-        int a = 5;
-        int b = 8;
         int subResult = calculator.sub(a, b);
         assertEquals(-3, subResult);
     }
 
     @Test
     public void testPow() {
-        Calculator calculator = new Calculator();
-        double a = 2;
-        double b = -2;
         double powResult = calculator.pow(a, b);
         assertEquals(Math.pow(a,b), powResult, 0.00001);
     }
+
     @Test
     public void testPow2() {
-        Calculator calculator = new Calculator();
-        double a = 2;
-        double b = 0;
         double powResult = calculator.pow(a, b);
         assertEquals(Math.pow(a,b), powResult, 0.00001);
     }
