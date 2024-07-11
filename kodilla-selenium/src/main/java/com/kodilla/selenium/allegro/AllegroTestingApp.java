1083d4033b038e479db06a7d637dcff99d1c6387
Adam Smok <adam.smok-pieniazek@outlook.com>
2024-07-11 20:11:38 +0200

    Zadanie 13_4

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -0,0 +1,19 @@
+package com.kodilla.selenium.allegro;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AllegroTestingApp {
+        public static void main(String[] args) {
+            System.setProperty("webdriver.chrome.driver", "C:/selenium-drivers/Chrome_Driver_x64/chromedriver-win64/chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://www.ebay.com/");
+            WebElement category = driver.findElement(By.name("_sacat"));
+            category.sendKeys("Cameras & Photo");
+            WebElement inputField = driver.findElement(By.name("_nkw"));
+            inputField.sendKeys("Mavic mini");
+            inputField.submit();
+        }
+    }
